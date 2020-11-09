30887bc26d53270447b64ad144b2bae9388556d9
rahalrazika <rahalraika@gmail.com>
2020-11-09 01:44:40 +0100

    style the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @articles = Article.all.is_most_recent
+    @article = @articles.first
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_attached_file :image, presence: true
   validates :title, presence: true, length: { in: 3..20 }
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 500 }
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
   scope :is_most_recent, -> { order(created_at: :desc) }
 
