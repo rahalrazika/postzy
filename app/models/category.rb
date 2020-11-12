c77aae224e614519903afdae6468fd2cc6aaebc5
rahalrazika <rahalraika@gmail.com>
2020-11-12 19:02:19 +0100

    fix the main img in the home page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,4 @@ class Category < ApplicationRecord
   has_many :categoryings, dependent: :destroy
   has_many :articles, through: :categoryings
   validates :name, presence: true, length: { in: 3..20 }
-
-  def to_s
-    name
-  end
 end
